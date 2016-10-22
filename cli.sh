e653f19619023b458cd0a81fdb77080c787410e5
Tyler Jewell <tjewell@codenvy.com>
2016-10-22 14:35:55 -0700

    restart

--- cli.sh
+++ cli.sh
@@ -826,10 +826,10 @@ cmd_config() {
     sed "s|^.*codenvy\.env.*$|\ \ \ \ \ \ \-\ \'${CODENVY_ENVFILE_CODENVY}\'|" -i "${REFERENCE_COMPOSE_FILE}"
     sed "s|^.*postgresql\/data.*$|\ \ \ \ \ \ \-\ \'codenvy-postgresql-volume\:\/var\/lib\/postgresql\/data\:Z\'|" -i "${REFERENCE_COMPOSE_FILE}"
 
-    text '' >> "${REFERENCE_COMPOSE_FILE}\n"
-    text 'volumes:' >> "${REFERENCE_COMPOSE_FILE}\n"
-    text '  codenvy-postgresql-volume:' >> "${REFERENCE_COMPOSE_FILE}\n"
-    text '     external: true' >> "${REFERENCE_COMPOSE_FILE}\n"
+    echo "" >> "${REFERENCE_COMPOSE_FILE}"
+    echo "volumes:" >> "${REFERENCE_COMPOSE_FILE}"
+    echo "  codenvy-postgresql-volume:" >> "${REFERENCE_COMPOSE_FILE}"
+    echo "     external: true" >> "${REFERENCE_COMPOSE_FILE}"
 
     # On Windows, it is not possible to volume mount postgres data folder directly
     # This creates a named volume which will store postgres data in docker for win VM
@@ -865,8 +865,8 @@ cmd_start() {
 
   # Begin tests of open ports that we require
   info "start" "Preflight checks"
-  text   "         port 80:  $(port_open 80 && echo \"${GREEN}[OK]${NC}\" || echo \"${RED}[ALREADY IN USE]${NC}\") \n"
-  text   "         port 443: $(port_open 443 && echo \"${GREEN}[OK]${NC}\" || echo \"${RED}[ALREADY IN USE]${NC}\") \n"
+  text   "         port 80:  $(port_open 80 && echo "${GREEN}[OK]${NC}" || echo "${RED}[ALREADY IN USE]${NC}") \n"
+  text   "         port 443: $(port_open 443 && echo "${GREEN}[OK]${NC}" || echo "${RED}[ALREADY IN USE]${NC}") \n"
   if ! $(port_open 80) || ! $(port_open 443); then
     error "Ports required to run codenvy are used by another program. Aborting..."
     return 1;
@@ -900,6 +900,20 @@ cmd_stop() {
 
 cmd_restart() {
   debug $FUNCNAME
+
+  # If container does not exist, we cannot restart.
+  if ! container_exist_by_name $CODENVY_SERVER_CONTAINER_NAME; then
+    info "restart" "Server is not running."
+    return
+  fi 
+
+  # If container exists, but server is not booted, we cannot restart
+  CURRENT_CODENVY_SERVER_CONTAINER_ID=$(get_server_container_id $CODENVY_SERVER_CONTAINER_NAME)
+  if ! container_is_running ${CURRENT_CODENVY_SERVER_CONTAINER_ID} || \
+     ! server_is_booted ${CURRENT_CODENVY_SERVER_CONTAINER_ID}; then
+     info "restart" "Server is not running." 
+     return
+  fi
 
   FORCE_UPDATE=${1:-"--no-force"}
   if [ "${FORCE_UPDATE}" == "--force" ]; then
